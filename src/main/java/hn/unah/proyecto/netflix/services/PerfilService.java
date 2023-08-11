48ba09f2d19b9008062a8825bc6ee636adbf368e
issc <issacjamaelc00@gmail.com>
2023-08-11 01:39:12 -0600

    verificar si el perfil esta ya en uso

--- src/main/java/hn/unah/proyecto/netflix/services/PerfilService.java
+++ src/main/java/hn/unah/proyecto/netflix/services/PerfilService.java
@@ -1,12 +1,13 @@
 package hn.unah.proyecto.netflix.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import hn.unah.proyecto.netflix.models.Perfil;
 
 public interface PerfilService {
     
-    public Perfil cambiarEstado(int idPerfil);
+    public Optional<Perfil> ingresarAlPerfil(int idPerfil);
 
     public List<Perfil> retornarPerfiles(int idUsuario);
 }
