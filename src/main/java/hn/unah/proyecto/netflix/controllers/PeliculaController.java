2931e496b31a56322e579f8fa0f2c26803db50ad
issc <issacjamaelc00@gmail.com>
2023-08-15 00:03:57 -0600

    swagger si no les funciona avisan

--- src/main/java/hn/unah/proyecto/netflix/controllers/PeliculaController.java
+++ src/main/java/hn/unah/proyecto/netflix/controllers/PeliculaController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,7 +29,7 @@ public class PeliculaController {
         return peliculaImplementacion.buscarPorCategoria(idCategoria);
     }
 
-    @PostMapping("/buscar/titulo")
+    @GetMapping("/buscar/titulo/{nombre}")
     public Optional<Pelicula> buscarPeliculas(@PathVariable String nombre) {
         return peliculaImplementacion.buscarPeliculas(nombre);
     }
